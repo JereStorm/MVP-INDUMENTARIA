c70ff7a22391c54021563900b263011bb514c839
Jeremias Storm <jerestorm@gmail.com>
2022-03-27 20:31:59 -0300

    responsive design para el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,107 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <header className='header navbar-light bg-light'>
-      
-      <nav className='navbar'>
-        <div className='container-fluid'>
-        <h1 className='text-center fst-italic title'>INDUMENTARIA <br /> MVP <br /> DEPORTIVA</h1>
-          <ul class="navbar-nav navegator ">
+    <header className="header bg-light d-flex align-items-center">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid gap-5">
+          <h1 className="text-center fst-italic title-header">
+            INDUMENTARIA <br /> MVP <br /> DEPORTIVA
+          </h1>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse gap-5" id="navbarScroll">
+            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarScrollingDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Productos
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarScrollingDropdown"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Sobre Nosotros
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Preguntas frecuentes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contacto
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      {/* <nav className='navbar navbar-expand-lg navbar-light'>
+        <div className='container-fluid '>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <h1 className='text-center fst-italic title-header'>INDUMENTARIA <br /> MVP <br /> DEPORTIVA</h1>
+          <div className='collapse navbar-collapse' id="navbarTogglerDemo01">
+            <ul class="navbar-nav navegator ">
             <li class="nav-item">
               <a class="nav-link active" href="#">Home</a>
             </li>
@@ -24,12 +118,14 @@ function App() {
               <a class="nav-link" href="#">Contacto</a>
             </li>
           </ul>
-          <form className='d-flex'>
+          <form className='d-flex mt-5'>
             <input className='form-control me-2' type="search" placeholder="Search" aria-label="Search"/>
             <button className='btn btn-outline-success' type="submit">Search</button>
           </form>
+          </div>
+          
         </div>
-      </nav>
+      </nav> */}
     </header>
   );
 }
