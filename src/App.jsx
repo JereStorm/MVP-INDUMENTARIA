4cdbb2e4e41bf4b05dfb414cee87b666c3e24b43
Jeremias Storm <jerestorm@gmail.com>
2022-04-03 16:25:48 -0300

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import AppRouter from "./routers/AppRouter";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <hr />
-      <section className="d-flex justify-content-center">
-        <img
-          src="https://scontent.faep36-2.fna.fbcdn.net/v/t1.6435-9/104496299_107978030967605_4984344705486710136_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeEFmwWjpm1PuiBdC-TzXKzuo5hUqpt-_OqjmFSqm3786nIHbF4_XJtLcaMidPlA4mFJXooXy1UgnhLhAWZGGqvS&_nc_ohc=hDZQ7yGhq4kAX_rVZIZ&_nc_ht=scontent.faep36-2.fna&oh=00_AT-gU-99HVMQ45qunUcTRlo64rayCT2sr4JwzTU9gWw01A&oe=62649BB8"
-          alt=""
-          className="img-home"
-        />
-      </section>
-      <hr />
-      <div>
-        <p className="section-title text-center">ULTIMOS INGRESOS</p>
-      </div>
+      <AppRouter />
     </div>
   );
 }
