8050be2b8775e434c1a99f07b5bdad2986273929
Jeremias Storm <jerestorm@gmail.com>
2022-04-03 18:03:56 -0300

    nav in react bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,209 +1,56 @@
 import React from "react";
-{
-  /* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid gap-5">
-          <h1 className="text-center fst-italic title-header">
-            INDUMENTARIA <br /> MVP <br /> DEPORTIVA
-          </h1>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse gap-5" id="navbarScroll">
-            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
+import { Nav, Navbar, NavbarBrand, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarScrollingDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Productos
-                </a>
-                <ul
-                  className="dropdown-menu"
-                  aria-labelledby="navbarScrollingDropdown"
-                >
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Sobre Nosotros
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Preguntas frecuentes
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contacto
-                </a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav> */
-}
 const Header = () => {
   return (
-    <header className="header bg-light d-flex align-items-center ">
-      <nav class="navbar navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-          <h1 className="text-center fst-italic mt-3">
-            <a class="navbar-brand title-header" href="#">
-              MVP INDUMENTARIA DEPORTIVA
-            </a>
+    // <header className="header bg-light d-flex align-items-center ">
+    <header>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+        bg="dark"
+        className="text-light"
+      >
+        <Navbar.Brand>
+          <h1 className="title-header text-uppercase font-italic m-3">
+            mvp indumentaria deportiva
           </h1>
-          <div className="gap-5">
-            <i class="bi bi-person-square btn btn-dark me-5"></i>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasNavbar"
-              aria-controls="offcanvasNavbar"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto ms-3">
+            <NavDropdown title="Admin" className="text-info">
+              <NavDropdown.Item href="/admin/users">Usuarios</NavDropdown.Item>
+            </NavDropdown>
 
-          <div
-            class="offcanvas offcanvas-end"
-            tabindex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
-                MENU
-              </h5>
-              <button
-                type="button"
-                class="btn-close text-reset"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="offcanvas-body">
-              <form class="d-flex">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 mt-3">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Sobre Nosotros
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Preguntas frecuentes
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Contacto
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarScrollingDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Productos
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarScrollingDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Accesorios
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Remeras
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Ofertas
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="Productos">
+              <NavDropdown.Item href="/products?remeras">
+                Remeras
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/products?pantalones">
+                Pantalones
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/products/camperas">
+                Camperas
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/products/buzos">Buzos</NavDropdown.Item>
+              <NavDropdown.Item href="/products/zapatillas">
+                Zapatillas
+              </NavDropdown.Item>
+              <NavDropdown.Item>Accesorios</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className="me-3">
+            <Nav.Link href="/login">Iniciar Sesion</Nav.Link>
+            <Nav.Link href="/register">Registrarse</Nav.Link>
+            <Nav.Link href="/account">Mi Cuenta</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </header>
   );
 };
